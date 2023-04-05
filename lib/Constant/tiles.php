55a282316a67d374e25d45aa8780cdcadedd77a5
kouki515 <71614240+kouki515@users.noreply.github.com>
2023-04-05 13:39:40 +0900

    add tiles const

---
+++ lib/Constant/tiles.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Constant;
+
+class tiles {
+    public $tiles = [
+        // 萬子
+        '一萬' => ['kind' => 'character', 'value' => 1],
+        '二萬' => ['kind' => 'character', 'value' => 2],
+        '三萬' => ['kind' => 'character', 'value' => 3],
+        '四萬' => ['kind' => 'character', 'value' => 4],
+        '五萬' => ['kind' => 'character', 'value' => 5],
+        '六萬' => ['kind' => 'character', 'value' => 6],
+        '七萬' => ['kind' => 'character', 'value' => 7],
+        '八萬' => ['kind' => 'character', 'value' => 8],
+        '九萬' => ['kind' => 'character', 'value' => 9],
+
+        // 筒子
+        '一筒' => ['kind' => 'circle', 'value' => 1],
+        '二筒' => ['kind' => 'circle', 'value' => 2],
+        '三筒' => ['kind' => 'circle', 'value' => 3],
+        '四筒' => ['kind' => 'circle', 'value' => 4],
+        '五筒' => ['kind' => 'circle', 'value' => 5],
+        '六筒' => ['kind' => 'circle', 'value' => 6],
+        '七筒' => ['kind' => 'circle', 'value' => 7],
+        '八筒' => ['kind' => 'circle', 'value' => 8],
+        '九筒' => ['kind' => 'circle', 'value' => 9],
+
+        // 索子
+        '一索' => ['kind' => 'bamboo', 'value' => 1],
+        '二索' => ['kind' => 'bamboo', 'value' => 2],
+        '三索' => ['kind' => 'bamboo', 'value' => 3],
+        '四索' => ['kind' => 'bamboo', 'value' => 4],
+        '五索' => ['kind' => 'bamboo', 'value' => 5],
+        '六索' => ['kind' => 'bamboo', 'value' => 6],
+        '七索' => ['kind' => 'bamboo', 'value' => 7],
+        '八索' => ['kind' => 'bamboo', 'value' => 8],
+        '九索' => ['kind' => 'bamboo', 'value' => 9],
+
+        // 字牌
+        '東' => ['kind' => 'honor', 'value' => 'east'],
+        '南' => ['kind' => 'honor', 'value' => 'south'],
+        '西' => ['kind' => 'honor', 'value' => 'west'],
+        '北' => ['kind' => 'honor', 'value' => 'north'],
+
+        '白' => ['kind' => 'honor', 'value' => 'whiteDragon'],
+        '發' => ['kind' => 'honor', 'value' => 'greenDragon'],
+        '中' => ['kind' => 'honor', 'value' => 'redDragon'],
+    ];
+}
