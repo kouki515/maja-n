dc48f6d3e68eefda911c60ca5a9d19377e4b335c
kouki515 <71614240+kouki515@users.noreply.github.com>
2023-04-05 16:57:46 +0900

    add deal logic

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php 
 
-phpinfo();
+require_once('../config/config.php');
+
+$mahjong = new \Controller\Mahjong();
+$mahjong->run();
+
+echo(count($mahjong->getDeck()));
+echo PHP_EOL;
+echo(count($mahjong->getHand()));
